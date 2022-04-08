f3003f2c113987e6db89288fef8485f78ace2ff2
Han Dongkyu <queuedq@gmail.com>
2022-04-08 18:35:25 +0900

    Change tab to spaces

--- src/canvas/index.ts
+++ src/canvas/index.ts
@@ -3,9 +3,9 @@ import { addMouseEvents } from './mouseEvents';
 import Scene from './Scene';
 
 export const init = ({ element, tools }) => {
-	const two = new Two({ autostart: true, fullscreen: true, domElement: element });
-	
-	const scene = new Scene(element, two.scene);
-	
-	addMouseEvents({ element, scene, tools });
+  const two = new Two({ autostart: true, fullscreen: true, domElement: element });
+  
+  const scene = new Scene(element, two.scene);
+  
+  addMouseEvents({ element, scene, tools });
 };
